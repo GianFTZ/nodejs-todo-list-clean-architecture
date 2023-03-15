651a05003f9740058dc45f24d0d8c4f5d675bc59
GianFTZ <gianfelipetz@hotmail.com>
2023-03-15 01:20:14 +0000

    test: ensure return 403 if no registered login is provided

---
+++ src/infra/repositories/login-repository.spec.ts
@@ -0,0 +1,37 @@
+import { LoginDto } from "../../domain/use-cases/dtos/loginDto"
+import { IAuthValidator } from "../contracts/auth-validator"
+import { loginRepository } from "./login-repository"
+
+const makeMock = () => {
+  class FakeAuthValidatorStub implements IAuthValidator {
+    async auth (loginDto: LoginDto): Promise<boolean>{
+      return true
+    }
+  }
+  return {
+    mockAuthValidator: new FakeAuthValidatorStub()
+  }
+}
+
+const makeSut = () => {
+  const { mockAuthValidator } = makeMock()
+  const sut = new loginRepository(mockAuthValidator)
+  return {
+    sut,
+    mockAuthValidator
+  }
+}
+
+describe('login repository', ()=> {
+  test('return 403 if login is not registered', async () => {
+    const fakeData = {
+      username: 'not_registered_username',
+      password: 'not_registered_password'
+    }
+    const { mockAuthValidator, sut } = makeSut()
+    const spy = jest.spyOn(mockAuthValidator, 'auth').mockResolvedValue(false)
+    const response = await sut.main(fakeData)
+    expect(response.status).toBe(403)
+  })
+  
+})
