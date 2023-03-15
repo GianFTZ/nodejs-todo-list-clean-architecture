4bbab2ff2857b087fee2563e264b5e3ec6f397da
GianFTZ <gianfelipetz@hotmail.com>
2023-03-15 12:57:23 +0000

    test: ensure return error if repository throws

---
+++ src/data/services/create-todo.spec.ts
@@ -0,0 +1,35 @@
+import { Http } from "../../utils/http/http"
+import { ICreateTodoRepository } from "../contracts/create-todo-repository"
+import { CreateTodoService } from "./create-todo"
+
+const makeMock = () => {
+  class FakeCreateTodoRepositoryStub implements ICreateTodoRepository {
+    async main (title: string, username: string): Promise<Http.Response>{
+      return {
+        status: 200,
+        body: { title }
+      }
+    }
+  }
+  return { 
+    mockCreateTodoRepository: new FakeCreateTodoRepositoryStub()
+  }
+}
+
+const makeSut = () => {
+  const { mockCreateTodoRepository } = makeMock()
+  const sut = new CreateTodoService(mockCreateTodoRepository)
+  return {
+    mockCreateTodoRepository,
+    sut
+  }
+}
+
+describe('create todo service', ()=> {
+  test('return 500 if repository throws', async () => {
+    const { mockCreateTodoRepository, sut } = makeSut()
+    const spy = jest.spyOn(mockCreateTodoRepository, 'main').mockResolvedValue({ status: 500, body: new Error('valid_error') })
+    const response = await sut.create({ title: 'valid_title' }, 'valid_username')
+    expect(response instanceof Error).toBe(true)
+  })
+})
