891c39a1043e505dec50a5927457bb408416661d
GianFTZ <gianfelipetz@hotmail.com>
2023-03-16 20:46:21 +0000

    feat: expose todo business rules to http

---
+++ src/main/routes/todo.ts
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import { CreateTodoService } from "../../data/services/create-todo";
+import { DoneTodoService } from "../../data/services/done-todo";
+import { PrismaCreateTodo } from "../../infra/adapters/prisma/create-todo";
+import { PrismaMarkAsDone } from "../../infra/adapters/prisma/mark-as-done";
+import { CreateTodoRepository } from "../../infra/repositories/create-todo-repository";
+import { DoneTodoRepository } from "../../infra/repositories/done-todo-repository";
+import { CreateTodoController } from "../../presentation/controllers/create-todo-controller";
+import { MarkTodoAsDoneController } from "../../presentation/controllers/markasdone-todo-controller";
+
+export default(router: Router) => {
+  // @create-to-do
+  router.post('/todo', async (req, res) => {
+    const { controller } = factoryCreate()
+    const httpResponse = await controller.handle(req)
+    res.status(httpResponse.status || 600).json(httpResponse.body)
+  })
+  // @mark-todo-as-done
+  router.put('/todo', async (req, res) => {
+    const { controller } = factoryDone()
+    const httpResponse = await controller.handle(req)
+    res.status(httpResponse.status || 600).json(httpResponse.body)
+  })
+}
+
+const factoryCreate = () => {
+  const creater = new PrismaCreateTodo()
+  const repository = new CreateTodoRepository(creater)
+  const service = new CreateTodoService(repository)
+  return {
+    controller: new CreateTodoController(service)
+  }
+}
+
+const factoryDone = () => {
+  const markasdone = new PrismaMarkAsDone()
+  const repository = new DoneTodoRepository(markasdone)
+  const service = new DoneTodoService(repository)
+  return {
+    controller: new MarkTodoAsDoneController(service)
+  }
+}
