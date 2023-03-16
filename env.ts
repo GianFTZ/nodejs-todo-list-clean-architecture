7cda9312cfe07dfe7a54c3a0844a8e226578d49d
GianFTZ <gianfelipetz@hotmail.com>
2023-03-16 17:07:34 +0000

    chore: add jwt

---
+++ env.ts
@@ -0,0 +1,4 @@
+export const env = {
+  port: process.env.PORT || 5000,
+  token: '6d4e9f1657204e27a61d4e9147753eb9ce3ef4704f0840555d012eb506cdffb1cb4cbec8cd4b7cecc6912222b65a0e2dfc85b2cbb4cab3b474c3127745d17b86'
+}
